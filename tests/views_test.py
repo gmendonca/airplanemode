9bad6255286043f52b85f9e41b833ba3b218b8f4
Gustavo Mendonca <gustavo@Gustavos-MacBook-Pro.local>
2016-07-13 23:21:59 -0300

    Added tests for Hello World

---
+++ tests/views_test.py
@@ -0,0 +1,13 @@
+import unittest
+from context import airplanemode
+
+class ViewsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = airplanemode.app.test_client()
+
+    def test_hello_word(self):
+        rv = self.app.get('/')
+        assert 'Hello World!' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
